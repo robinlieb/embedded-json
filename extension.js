9b1644be7b40ecf52a4029be87b47d1ffb13bf7a
Robin Lieb <robin.j.lieb@gmail.com>
2022-05-05 19:59:05 +0200

    feat: added command to release embedded JSON in entire document

--- extension.js
+++ extension.js
@@ -5,8 +5,29 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('embedded-json.helloWorld', function () {
-		vscode.window.showInformationMessage('Hello World from Embedded JSON!');
+	let disposable = vscode.commands.registerCommand('embedded-json.releaseEmbeddedJSON', function () {
+
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+
+		const document = editor.document;
+		const text = document.getText();
+
+		const fullRange = new vscode.Range(
+			document.positionAt(0),
+			document.positionAt(text.length)
+		)
+
+		var alteredText = text.replaceAll('\"{', '{');
+		alteredText = alteredText.replaceAll('}\"', '}');
+		alteredText = alteredText.replaceAll('\\"', '"');
+
+		editor.edit(
+			(editBuilder) => {
+				editBuilder.replace(fullRange, alteredText);
+			});
 	});
 
 	context.subscriptions.push(disposable);
