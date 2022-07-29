07cc85a0014eaf441b61cc4d7a07637e211a70d8
Robin Lieb <robin.j.lieb@gmail.com>
2022-07-29 21:49:54 +0200

    test: added test for vscode command

--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -1,4 +1,6 @@
 const assert = require('assert');
+const path = require('path');
+const expectedJson = require("../unit/expected.json");
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -8,8 +10,23 @@ const vscode = require('vscode');
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	teardown(async () => {
+		await vscode.commands.executeCommand('workbench.action.closeAllEditors');
+	});
+
+	test('Test executing command', () => {
+		return vscode.workspace.openTextDocument(path.resolve(__dirname, '../unit', "input.json"))
+			.then((doc) => {
+				return vscode.window.showTextDocument(doc);
+			})
+			.then(editor => {
+				return vscode.commands.executeCommand('embedded-json.unwrapEmbeddedJSON')
+				.then(() => {
+					var text = editor.document.getText()
+					text = text.replaceAll('\r\n', '\n');
+					var exp = JSON.stringify(expectedJson, null, 4)
+					assert.equal(text, exp)
+				})
+			})
 	});
 });
